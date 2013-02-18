ec07e758725c0de5f6418f190b35aa37fb18a2db
Olaf Klischat <olaf.klischat@gmail.com>
2013-02-18 02:45:37 +0100

    emacs: groovy-mode for .gradle files

--- .xemacs/init.el
+++ .xemacs/init.el
@@ -252,6 +252,7 @@
  ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
  (autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
  (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+ (add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
  (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode)))
 
 (initialize-lib
