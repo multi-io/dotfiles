f989fe3c29675e43116f52fecfeb2839d4b701e0
Olaf Klischat <o.klischat@syseleven.de>
2018-04-17 23:51:29 +0200

    emacs: latest settings changes

--- .xemacs/custom.el
+++ .xemacs/custom.el
@@ -1,20 +1,24 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(canlock-password "6fc929d2b0cdde00057f2bbe0e1367dba94f5034")
  '(case-fold-search t)
  '(column-number-mode t)
  '(gnuserv-program (concat exec-directory "/gnuserv"))
  '(line-number-mode t)
+ '(package-selected-packages
+   (quote
+    (markdown-mode+ markdown-mode yaml-mode puppet-mode php-mode inf-ruby go-mode go-autocomplete 2048-game)))
  '(query-user-mail-address nil)
+ '(require-final-newline nil)
  '(tab-width 4)
  '(toolbar-visible-p nil)
  '(user-mail-address "olaf@tack"))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
