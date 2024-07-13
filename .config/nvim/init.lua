fd709f76128c9cb172863cdce091477cd669c769
Olaf Klischat <olaf.klischat@gmail.com>
2024-07-13 13:03:48 +0200

    nvim: +luasnip
    nvim-cmp requires a snippet engine. This fulfils that requirement.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -159,6 +159,18 @@ require("lazy").setup({
     },
 
     {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+    },
+
+    'saadparwaiz1/cmp_luasnip',
+
+    'rafamadriz/friendly-snippets',
+
+    {
         "neovim/nvim-lspconfig",
         dependencies = {
             'williamboman/mason.nvim',
@@ -239,6 +251,8 @@ require("lazy").setup({
                 },
             })
 
+            require("luasnip.loaders.from_vscode").lazy_load()
+
             local cmp = require('cmp')
 
             local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -273,7 +287,21 @@ require("lazy").setup({
                     {
                         name = "path"
                     },
-                })
+                    {
+                        name = "luasnip"
+                    },
+                }),
+
+                snippet = {
+                    -- REQUIRED - you must specify a snippet engine
+                    expand = function(args)
+                        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                    end,
+                }
             }
 
             vim.diagnostic.config({
