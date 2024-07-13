314b022c88ea42d3cd253ca393fe9096fffb17c1
Olaf Klischat <olaf.klischat@gmail.com>
2024-07-13 16:10:12 +0200

    nvim: no automatic newlines at EOF

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,6 +10,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.showmode = false
+vim.opt.fixendofline = false
 
 vim.opt.number = true
 vim.opt.relativenumber = true
