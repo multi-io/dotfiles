41ea3f3fb94ca8fe137f2d07c49925e4e6548b1e
Olaf Klischat <olaf.klischat@gmail.com>
2024-07-22 02:27:40 +0200

    nvim: lsp keymaps, lsp_signature

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -172,6 +172,13 @@ require("lazy").setup({
     'rafamadriz/friendly-snippets',
 
     {
+        "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        opts = {},
+        config = function(_, opts) require'lsp_signature'.setup(opts) end
+    },
+
+    {
         "neovim/nvim-lspconfig",
         dependencies = {
             'williamboman/mason.nvim',
@@ -183,6 +190,36 @@ require("lazy").setup({
             "hrsh7th/nvim-cmp",
         },
         config = function()
+            local function on_attach(client, bufnr)
+                -- Set up buffer-local keymaps (vim.api.nvim_buf_set_keymap()), etc.
+                local opts = { noremap = true, silent = true }
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "v", "<C-k>", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+                -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>s", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+                -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+                -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "single" })<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "single" })<CR>', opts)
+                -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+                vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+
+                require 'lsp_signature'.on_attach({
+                    bind = true,
+                    floating_window_above_cur_line = true,
+                    max_width = 120,
+                    hi_parameter = 'Cursor',
+                    hint_enable = false,
+                    handler_opts = {
+                        border = 'single'
+                    }
+                }, bufnr)
+            end
+
             -- just set all dependencies up in here as well
             -- require('nvim-lspconfig')  -- nvim-lspconfig itself doesn't have a module?
             require('mason').setup()
@@ -208,12 +245,17 @@ require("lazy").setup({
                 -- see :h mason-lspconfig.setup_handlers()
                 handlers = {
                     function(server_name)
-                         -- default (fallback) handler
-                        require('lspconfig')[server_name].setup{}
+                        -- default (fallback) handler
+                        local opts = {
+                            on_attach = on_attach,
+                        }
+                        require('lspconfig')[server_name].setup(opts)
                     end,
 
                     ['lua_ls'] = function()
                         require('lspconfig').lua_ls.setup {
+                            on_attach = on_attach,
+
                             -- from :help lspconfig-all /lua_ls
                             -- TODO investigate https://github.com/folke/neodev.nvim
                             on_init = function(client)
