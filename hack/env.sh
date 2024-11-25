c60375210527b2dbcb8749e7d1b66e85d00bc4db
Olaf Klischat <olaf.klischat@gmail.com>
2024-11-25 03:43:16 +0100

    hack/env upd

--- hack/env.sh
+++ hack/env.sh
@@ -1,2 +1,2 @@
-FILTER='^(.config/|.bash|.zsh|.git|.irbrc|.shrc$|.emacs|.xemacs|emacs-lisp|.ssh|.tmux|.vim|.profile|.gitignore|hack|Library)'
+FILTER='^(.config/|.bash|.zsh|.git|.irbrc|.shrc$|.emacs|.xemacs|emacs-lisp|.tmux|.vim|.profile|.gitignore|hack|Library)'
 
